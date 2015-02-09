1f7194cc6a9cbbdc9ee120f762e8c3f26808bb24
Allison Higginbotham <higgintop@gmail.com>
2015-02-09 15:46:47 -0500

    Fixed IE10 sauce lab testing issue

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -81,7 +81,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['SL_IE10'],
+    browsers: ['PhantomJS', 'Firefox', 'SL_IE10'],
 
 
     // Continuous Integration mode
